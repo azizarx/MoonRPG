5daf9d310b0a55668d18dd10bed235e71b5b91db
Mohamed Aziz Nabli <aziznabli7790@gmail.com>
2024-09-13 13:25:08 +0100

    working on the UI of the Inventory

--- Assets/_Scripts/Player/Inventory.cs
+++ Assets/_Scripts/Player/Inventory.cs
@@ -1,18 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Inventory : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public Image[] slotsImages;
+    public List<Item> items = new List<Item>();
+    public int space = 8;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+   
 }
